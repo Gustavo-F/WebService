33f9ede83482c22acfd04fadd997f5a92545b5c2
Gustavo-F <fgustavo016@gmail.com>
2021-11-06 22:45:49 -0300

    inplement radom weather and statistics post to server

--- Clients/CollectorClient.py
+++ Clients/CollectorClient.py
@@ -1,7 +1,8 @@
+import os
 import json
 import time
-import socket
 import random
+import requests
 
 def random_date(start, end, format, prop):
     end_time = time.mktime(time.strptime(end, format))
@@ -19,22 +20,36 @@ def random_hour():
             f'{"0" + str(minute) if minute < 10 else minute}:'
             f'{"0" + str(second) if second < 10 else second}')
 
-def Main():
-    host = '127.0.0.1'
-    port = random.randrange(2000, 7999)
+def random_weather():
+    value = str(random.randint(1, 3))
+    return {
+        '1': 'Rainy',
+        '2': 'Sunny',
+        '3': 'Cloudy',
+    }[value]
 
-    collector_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    collector_socket.bind((host, port))
+def send_statistics(client, meteorological_statistics):
+    url = 'http://127.0.0.1:8000/add_statistic/'
+    
+    response = client.post(url, data=dict(
+        statistics=meteorological_statistics,
+        next='/',
+    ))
+    
+    print(response)
+
+def Main():
+    client = requests.session()
 
     while(True):
         meteorological_statistics = {
-            'weather': 'rain',
+            'weather': random_weather(),
             'temperature': round(random.uniform(-5, 40), 1),
-            'date': random_date('01/01/2012', '05/11/2021', '%d/%m/%Y', random.random()),
+            'date': random_date('2021-01-01', '2021-11-01', '%Y-%m-%d', random.random()),
             'hour': random_hour(),
         }            
 
-        print(json.dumps(meteorological_statistics))
+        send_statistics(client, json.dumps(meteorological_statistics))
         time.sleep(10)
 
 if __name__ == '__main__':
