5d7d87e061607f1534672a36f82156b740bf2dfd
Gustavo-F <fgustavo016@gmail.com>
2021-11-09 14:53:00 -0300

    Added function to put loaded data on HTML table

--- Clients/ViewClient/src/index.js
+++ Clients/ViewClient/src/index.js
@@ -1,6 +1,7 @@
 $('#datepicker').datepicker({
     format: 'yyyy-mm-dd',
     todayBtn: true, 
+    todayHighlight: true, 
 });
 
 $(document).ready(() => {
@@ -21,10 +22,44 @@ function getStatistics(date) {
         dataType: 'json',
         data: {date: date},
         success: (response) => {
-            console.log(response);
+            putOnTable(response);
         },
         error: (response) => {
             console.log(`Error => ${response}`);
         }     
     });
+}
+
+function putOnTable(statistics) {
+    $('#statisticsTable tbody tr').remove();
+    
+    var tableBody = document.getElementById('statisticsTableBody');
+
+    for(i = 0; i < statistics.length; i++) {
+        var current = statistics[i];
+        var newRow = document.createElement('tr');
+
+        for(var key in current) {
+            var newCell = document.createElement('td');
+            newCell.innerText = current[key];
+
+            newRow.appendChild(newCell);
+        }
+
+        var editCell = document.createElement('button');
+        editCell.setAttribute('class', 'btn btn-warning');
+        editCell.innerHTML = '<i class="fas fa-edit text-white"></i>';
+        var newCell = document.createElement('td');
+        newCell.appendChild(editCell)
+        newRow.appendChild(newCell);
+
+        var deleteCell = document.createElement('button');
+        deleteCell.setAttribute('class', 'btn btn-danger');
+        deleteCell.innerHTML = '<i class="fas fa-trash-alt"></i>';
+        var newCell = document.createElement('td');
+        newCell.appendChild(deleteCell)
+        newRow.appendChild(newCell);
+
+        tableBody.appendChild(newRow);
+    }
 }
